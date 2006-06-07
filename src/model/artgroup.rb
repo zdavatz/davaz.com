ed0518111f919fffeb43b634e277699016679b0f
Markus Huggler <mhuggler@ywesee.com>
2006-06-07 14:59:10 +0200

    added one more file

---
+++ src/model/artgroup.rb
@@ -0,0 +1,10 @@
+#!/usr/bin/env ruby
+# Model::Artgroup -- davaz.com -- 06.06.2006 -- mhuggler@ywesee.com
+
+module DAVAZ
+	module Model
+		class Artgroup
+			attr_accessor :artgroup_id, :name, :shop_order
+		end
+	end
+end

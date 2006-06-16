a8a066e7c98790c0bd477a5b7da79a959ba3d5f4
Markus Huggler <mhuggler@ywesee.com>
2006-06-16 18:07:02 +0200

    Added serie_widget, removed slideshow.rb

--- src/view/serie_widget.rb
+++ src/view/serie_widget.rb
@@ -7,26 +7,21 @@ require 'util/image_helper'
 
 module DAVAZ
 	module View
-		class Slideshow < HtmlGrid::Component
-			def to_html(context)
-				args = {
+		class SerieWidget < HtmlGrid::NamedComponent
+			def compose_args
+				{
 					'images'	=> [],
 					'titles'	=> [],
 					'transitionInterval'	=>	'1500',
 					'delay'		=> '3500',
 					'imageHeight'	=>	SLIDESHOW_IMAGE_HEIGHT,
+					#'id'			=>	@name.to_s,
+					'id'			=>	'show',
 				}
-				model.each { |slide|
-					image = DAVAZ::Util::ImageHelper.image_path(slide.display_id, 'slideshow')
-					if(slide.title=="?")
-						title = "no title" 
-					else
-						title = slide.title
-					end
-					args['images'].push(image)
-					args['titles'].push(title)
-				}
-				dojo_tag('slideshow', args)
+			end
+			def to_html(context)
+				args = compose_args.update(@model)
+				dojo_tag(@name, args)
 			end
 		end
 	end

cd46bda8a863d932871123debb7503223c5c75e4
Markus Huggler <mhuggler@ywesee.com>
2006-09-20 14:50:20 +0200

    Renamed guestbook_entry file

--- src/view/communication/guestbook_entry.rb
+++ src/view/communication/guestbook_entry.rb
@@ -22,7 +22,7 @@ end
 class GuestbookEntryForm < View::Form
 	include HtmlGrid::ErrorMessage
 	LABELS = true
-	EVENT = :add_entry
+	EVENT = :submit_entry
 	CSS_CLASS = 'guestbookentry-form'
 	COMPONENTS = {
 		[0,0]	=>	:name,
@@ -33,51 +33,39 @@ class GuestbookEntryForm < View::Form
 		[0,3]	=>	:messagetxt,
 		[1,4]	=>	:submit,
 	}
+	COLSPAN_MAP = {
+		[1,3]	=>	3,
+	}
 	CSS_MAP = {
 		[1,4]	=>	'add-entry',
 	}
 	SYMBOL_MAP = {
 		:name			=>	HtmlGrid::InputText,
 		:surname	=>	HtmlGrid::InputText,	
-		:email		=>	HtmlGrid::InputText,	
 		:city			=>	HtmlGrid::InputText,	
 		:country	=>	HtmlGrid::InputText,	
-	}
-	COLSPAN_MAP = {
-		[1,3]	=>	4,
-		[1,4]	=>	3,
 	}
 	def init
 		super
 		error_message
+		self.onsubmit = 'return false;'
 	end
 	def messagetxt(model)
 		input = HtmlGrid::Textarea.new(:messagetxt, model, @session, self)
-		input.set_attribute('cols', 70)
+		input.set_attribute('cols', 42)
 		input.set_attribute('rows', 10)
 		input.set_attribute('wrap', true)
-		#js = "if(this.value.length > 400) { (this.value = this.value.substr(0,400))}" 
-		#input.set_attribute('onKeypress', js)
+		input.set_attribute('style', 'border: 1pt lightgrey inset;')
 		input.label = true
 		input
 	end
 	def submit(model)
-		button = HtmlGrid::Button.new(:add_entry, model, @session, self)
-		button.value = @lookandfeel.lookup(:add_entry)
+		button = HtmlGrid::Button.new(:submit_entry, model, @session, self)
+		button.value = @lookandfeel.lookup(:submit_entry)
 		button.css_class = 'add-entry' 
 		button.attributes['type'] = 'submit'
 		button
 	end
-end
-class GuestbookEntryComposite < HtmlGrid::DivComposite
-	CSS_CLASS = 'content'
-	COMPONENTS = {
-		[0,0]	=>	GuestbookEntryTitle,
-		[1,0]	=>	GuestbookEntryForm,
-	}
-end
-class GuestbookEntry < View::CommunicationPublicTemplate
-	CONTENT = View::Communication::GuestbookEntryComposite
 end
 		end
 	end

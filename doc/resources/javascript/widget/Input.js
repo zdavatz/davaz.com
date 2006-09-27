44d0bee26cb231370a938be435a10f36a88169d6
Markus Huggler <mhuggler@ywesee.com>
2006-09-27 10:43:09 +0200

    DEBUGGING

--- doc/resources/javascript/widget/Input.js
+++ doc/resources/javascript/widget/Input.js
@@ -126,7 +126,7 @@ dojo.widget.defineWidget(
 			var intIndexOfMatch = strText.indexOf( strTarget );
 
 			while (intIndexOfMatch != -1){
-				strText = strText.replace( strTarget, strSubString )
+				strText = strText.replace( strTarget, strSubString );
 				intIndexOfMatch = strText.indexOf( strTarget );
 			}
 			return( strText );

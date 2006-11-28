zdavatz/davaz.com

commit ea7ae1101f1cc9bffc623621ca329aab831aab18

Don't display nameless series

src/view/serie_links.rb
@@ -25,10 +25,12 @@ module DAVAZ
       def series(model, target)
         res = []
         model.series.each { |name|
-          unless(res.empty?)
-            res.push(', ')
+          unless(name.strip.empty?)
+            unless(res.empty?)
+              res.push(', ')
+            end
+            res.push(serie_link(name, target))
           end
-          res.push(serie_link(name, target))
         }
         res
       end

commit 288da18b1811161a1f68ccbcc6cf02184b99a3e7

Fixed bug introduced with ea7ae1101f1cc9bffc623621ca329aab831aab18

src/view/serie_links.rb
@@ -24,12 +24,12 @@ module DAVAZ
 			end
       def series(model, target)
         res = []
-        model.series.each { |name|
-          unless(name.strip.empty?)
+        model.series.each { |serie|
+          unless(serie.name.to_s.strip.empty?)
             unless(res.empty?)
               res.push(', ')
             end
-            res.push(serie_link(name, target))
+            res.push(serie_link(serie, target))
           end
         }
         res
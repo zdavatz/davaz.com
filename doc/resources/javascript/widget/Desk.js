5471966b3a033325c99f3dcae6b45a36d2e58933
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-07-08 16:32:49 +0200

    Make Desk widget to work

--- doc/resources/javascript/widget/Desk.js
+++ doc/resources/javascript/widget/Desk.js
@@ -1,50 +1,40 @@
-dojo.provide("ywesee.widget.Desk");
-
-dojo.require("dijit._Widget");
-dojo.require("dijit._Templated");
-
-dojo.declare(
-  "ywesee.widget.Desk",
-	[dijit._Widget, dijit._Templated],
-  {
-
-    templatePath: dojo.moduleUrl("ywesee.widget", "templates/HtmlDesk.html"),
-
-    view: "Desk",
-
-    images: [],	
-    titles: [],
-    dataUrl: "",
-    serieId: "",
-    deskContainer: null,
-    deskContent: null,
-    contentPane: null,
-
-    startup: function(){
+define([
+  'dojo/_base/declare'
+, 'dojo/_base/xhr'
+, 'dojo/ready'
+, 'dojo/parser'
+, 'dijit/_WidgetBase'
+, 'dijit/_TemplatedMixin'
+], function(declare, xhr, ready, parser, _wb, _tm) {
+  declare('ywesee.widget.Desk', [_wb, _tm], {
+    templatePath: require.toUrl(
+      '/resources/javascript/widget/templates/HtmlDesk.html')
+  , view:          'Desk'
+  , images:        []
+  , titles:        []
+  , dataUrl:       ''
+  , serieId:       ''
+  , deskContainer: null
+  , deskContent:   null
+  , contentPane:   null
+  , startup:       function() {
       var newDataUrl = this.dataUrl.replace(/ajax_rack/, 'ajax_desk');
-
       _this = this;
-
-      console.debug("Desk.startup, getting: ", newDataUrl);
-      dojo.xhrGet({
-        url: newDataUrl,
-        load: function(data, request) {
+      xhr.get({
+        url:      newDataUrl
+      , handleAs: 'text'
+      , load:     function(data, request) {
           _this.toggleInnerHTML(data);
-        },
-        error: function(data, request) { 
-          console.debug("could not load Desk!", data, request); 
-        },
-        handleAs: "text"
+        }
       });
-    },
-
-    toggleInnerHTML: function(html) {
+    }
+  , toggleInnerHTML: function(html) {
       this.deskContent.innerHTML = html;
       try {
-        dojo.parser.parse(this.deskContent);
+        parser.parse(this.deskContent);
       } catch(e) {
-        console.debug("could not parse desk content!"); 
+        // pass
       }
     }
-  }
-);
+  });
+});

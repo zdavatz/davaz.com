256ee141c05f94e596cea9664747a24ffd04ffac
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-07-13 12:30:01 +0200

    Fix admin ajax form update for art object

--- src/state/gallery/result.rb
+++ src/state/gallery/result.rb
@@ -1,15 +1,12 @@
-#!/usr/bin/env ruby
-# State::Gallery::Search -- davaz.com -- 06.06.2006 -- mhuggler@ywesee.com
-
 require 'state/global_predefine'
 require 'view/gallery/result'
 
 module DAVAZ
-	module State
-		module Gallery
-class Result < State::Gallery::Global
-	VIEW = View::Gallery::Result	
-end
-		end
-	end
+  module State
+    module Gallery
+      class Result < State::Gallery::Global
+        VIEW = View::Gallery::Result
+      end
+    end
+  end
 end

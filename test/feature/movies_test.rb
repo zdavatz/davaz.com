7323dff4f971192b70a0ef0d366170f52e10cadf
Yasuhiro Asaka <grauwoelfchen@gmail.com>
2016-08-18 19:02:23 +0200

    Fix updating failure on editor at movie admin form

--- test/feature/movies_test.rb
+++ test/feature/movies_test.rb
@@ -72,61 +72,30 @@ class TestMovies < Minitest::Test
     assert_match('Item 1 of 5', pager.text)
   end
 
-  #def test_stub
-  #  @selenium.open "/en/works/multiples/#112"
-  #  @selenium.wait_for_page_to_load "30000"
-  #end
+  def test_admin_movies_update_description_text_by_wysiwyg_editor
+    assert_match('/en/works/movies', browser.url)
 
-  #def test_test_works_views
-  #  @selenium.open "/en/communication/links/"
-  #  @selenium.click "link=Drawings"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Drawings", @selenium.get_title
-  #  @selenium.mouse_over "112"
-  #  sleep 1
-  #  assert @selenium.is_text_present("Title of ArtObject 112")
-  #  assert @selenium.is_text_present("Name of Serie ABC, Name of Serie ABD")
-  #  @selenium.click "link=Paintings"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Paintings", @selenium.get_title
-  #  assert @selenium.is_text_present("Title of ArtObject 111")
-  #  @selenium.click "link=Multiples"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Multiples", @selenium.get_title
-  #  assert @selenium.is_text_present("Multiples")
-  #  @selenium.click "//img[@name='111']"
-  #  sleep 2
-  #  @selenium.click "link=Movies"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Movies", @selenium.get_title
-  #  assert @selenium.is_text_present("Title of ArtObject 115")
-  #  @selenium.click "link=more..."
-  #  sleep 5
-  #  begin
-  #      assert @selenium.is_text_present("Text of ArtObject 111")
-  #  rescue Test::Unit::AssertionFailedError
-  #      @verification_errors << $!
-  #  end
-  #  @selenium.click "paging_next"
-  #  begin
-  #      assert @selenium.is_text_present("Text of ArtObject 112")
-  #  rescue Test::Unit::AssertionFailedError
-  #      @verification_errors << $!
-  #  end
-  #  @selenium.click "link=Back To Overview"
-  #  sleep 5
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Movies", @selenium.get_title
-  #  @selenium.click "link=Photos"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Photos", @selenium.get_title
-  #  assert @selenium.is_text_present("Title of ArtObject 111")
-  #  @selenium.click "link=Design"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Design", @selenium.get_title
-  #  assert @selenium.is_text_present("Design")
-  #  @selenium.click "link=Schnitzenthesen"
-  #  @selenium.wait_for_page_to_load "30000"
-  #  assert_equal "Da Vaz - Abstract Artist from Switzerland | Works | Schnitzenthesen", @selenium.get_title
-  #  assert @selenium.is_text_present("Title of ArtObject 111")
-  #end
+    link = browser.link(:name, '111-more')
+    link.click
+
+    login_as(email: 'right@user.ch', password: 'abcd')
+
+    frame = wait_until { browser.iframe(:index, 0) }
+    editor = frame.div(:id, 'dijitEditorBody')
+    editor.focus
+    editor.send_keys('UPDATED: ')
+    editor.send_keys(:tab)
+    assert_equal('UPDATED: Text of ArtObject 111', editor.text)
+
+    # TODO :'(
+    #button = browser.element(name: 'update')
+    #button.click
+
+    #view = wait_until { browser.div(:id, 'movies_gallery_view') }
+    #assert_match('/en/works/movies/#111', browser.url)
+
+    #frame = wait_until { browser.iframe(:index, 0) }
+    #editor = frame.div(:id, 'dijitEditorBody')
+    #assert_equal('UPDATED: Text of ArtObject 111', editor.text)
+  end
 end

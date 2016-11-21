0fd16ae631607e9e69e74acb88bd8a34033803a3
Niklaus Giger <ngiger@ywesee.com>
2016-11-21 15:38:34 +0100

    Run all tests. Fail if unable to login

--- test/support/authentication.rb
+++ test/support/authentication.rb
@@ -11,12 +11,14 @@ module DaVaz
       form.text_field(name: 'login_password').set(opts[:password])
       form.checkbox(name: 'remember_me').set
       form.button(name: 'login', type: 'submit').click
+      assert_equal(false,  browser.text_field(:name => "login_email").visible?, 'Could not log in. Login-Field must no longer be present!')
     end
 
     def logout
       foot_container = wait_until { browser.div(id: 'foot_container') }
       logout_link = foot_container.a(name: 'logout')
       logout_link.click
+      assert_equal(true,  browser.text_field(:name => "login_email").visible?, 'Login must be present!')
     end
   end
 end

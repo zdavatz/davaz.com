2355aa81a4ef4d63dcd2e42da4ad53f25cc7f529
Niklaus Giger <ngiger@ywesee.com>
2021-03-12 08:28:27 +0100

    (#13) Unable to edit page

--- src/util/pw_server.rb
+++ src/util/pw_server.rb
@@ -1,21 +1,29 @@
-require 'yaml'
+require "yaml"
 class PwServer
-    
   PwEntry = Struct.new(:mail, :password, :token) do
     def show
-      YAML.dump(self)     # 
+      YAML.dump(self)
     end
+
     def valid?
       true
     end
+
     def allowed?(capability, domain)
       puts "allowed? called with #{capability} and #{domain}" if $VERBOSE
       true
     end
+
+    def generate_token
+      token.to_s
+    end
+
+    def name
+      mail
+    end
   end
   def initialize
-    PwServer.read_etc_files
-    @entries = YAML.load(File.read(@@password_file))
+    @entries = YAML.safe_load(File.read(@@password_file), [Symbol, PwEntry])
   end
 
   def valid?
@@ -24,50 +32,50 @@ class PwServer
 
   def login(email, password)
     crypted = password.crypt(@@mySalt)
-    @entries.each do | entry|
+    @entries.each do |entry|
       if entry.mail.eql?(email) && entry.password.eql?(crypted)
         puts "Got #{entry.token} to login via  #{email} #{password}" if $VERBOSE
         return entry
       end
     end
     puts "Unable to login via  #{email} #{password}" if $VERBOSE
-    return false
+    false
   end
-  
+
   def login_token(email, token)
-    @entries.each do | entry|
-      if entry.mail.eql?(email) && entry.token.to_i==token.to_i
-        puts "Got #{entry.token} to login_token via  #{email} #{token}"  if $VERBOSE
+    @entries.each do |entry|
+      if entry.mail.eql?(email) && entry.token.to_i == token.to_i
+        puts "Got #{entry.token} to login_token via  #{email} #{token}" if $VERBOSE
         return entry
       end
     end
     puts "Unable to login_token via  #{email} #{token}" if $VERBOSE
-    return false
+    false
   end
-  
+
   def logout
     false
   end
-  
-private 
-  def PwServer.read_etc_files
-    password_file = File.join(Dir.pwd, defined?(MiniTest) ? 'test':'etc', 'pw_server.passwords')
-    salt_file = File.join(Dir.pwd, defined?(MiniTest) ? 'test':'etc', 'pw_server.salt')
-    unless File.exists?(password_file)
+
+  private_class_method
+  def self.readEtcFiles
+    password_file = File.join(Dir.pwd, defined?(MiniTest) ? "test" : "etc", "pw_server.passwords")
+    salt_file = File.join(Dir.pwd, defined?(MiniTest) ? "test" : "etc", "pw_server.salt")
+    unless File.exist?(password_file)
       puts "Missing password file  #{password_file}"
       exit(3)
     end
-    unless File.exists?(salt_file)
+    unless File.exist?(salt_file)
       puts "Missing salt file  #{salt_file}"
       exit(3)
     end
     @@mySalt = File.readlines(salt_file).first
     @@password_file = password_file
   end
-  PwServer.read_etc_files
+  PwServer.readEtcFiles
 
-# helper for generating password
-  def PwServer.generate_pw_entry(email, password)
+  # helper for generating password
+  def self.generate_pw_entry(email, password)
     entry = PwEntry.new(email, password)
     entry.password = entry.password.crypt(@@mySalt)
     entry.token = rand(9999999999999999999)
